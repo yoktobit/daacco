eef0e9f9097dad3141c6c296b6bd118b494753f5
Martin Windolph <martin@yoktobit.de>
2021-04-08 22:43:03 +0200

    Unit tests

--- daacco-r2dbc/src/main/java/de/yoktobit/daacco/r2dbc/foomanagement/dataaccess/api/repo/BarRepository.java
+++ daacco-r2dbc/src/main/java/de/yoktobit/daacco/r2dbc/foomanagement/dataaccess/api/repo/BarRepository.java
@@ -3,9 +3,20 @@ package de.yoktobit.daacco.r2dbc.foomanagement.dataaccess.api.repo;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
 import de.yoktobit.daacco.r2dbc.foomanagement.dataaccess.api.BarEntity;
+
 import reactor.core.publisher.Mono;
 
+/**
+ * Repo für BarEntities
+ *
+ */
 public interface BarRepository extends ReactiveCrudRepository<BarEntity, Long> {
-    
-    Mono<BarEntity> findByName(String name);
+
+  /**
+   * Sucht ein {@link BarEntity} nach Name
+   *
+   * @param name Name der BarEntity
+   * @return {@link BarEntity}
+   */
+  Mono<BarEntity> findByName(String name);
 }
